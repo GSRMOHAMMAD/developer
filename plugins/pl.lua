0c7ffdb4d51d89d266c53c66cf8e0fd4f00fea9c
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-07-20 00:56:08 -0800

    Update pl.lua

--- plugins/pl.lua
+++ plugins/pl.lua
@@ -25,14 +25,14 @@ local function list_plugins(only_enabled)
   local text = ''
   for k, v in pairs( plugins_names( )) do
     --  ➗ enabled, ➖ disabled
-    local status = '➖'
+    local status = '❎'
     -- Check if is enabled
     for k2, v2 in pairs(_config.enabled_plugins) do
       if v == v2..'.lua' then 
-        status = '➗' 
+        status = '✅' 
       end
     end
-    if not only_enabled or status == '➗' then
+    if not only_enabled or status == '✅' then
       -- get the name
       v = string.match (v, "(.*)%.lua")
       text = text..v..'  '..status..'\n'
